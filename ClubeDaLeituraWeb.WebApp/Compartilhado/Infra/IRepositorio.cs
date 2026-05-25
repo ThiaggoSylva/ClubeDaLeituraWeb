c33d44b3c0f5e6923bb293f353a3f568d4e4dd69
Tiago Santini <tiagosantiniathayde@outlook.com>
2026-05-25 14:03:46 -0300

    Adicionado arquivos base de domínio e infra

---
+++ ClubeDaLeituraWeb.WebApp/Compartilhado/Infra/IRepositorio.cs
@@ -0,0 +1,13 @@
+using ClubeDaLeituraWeb.WebApp.Compartilhado.Dominio;
+
+namespace ClubeDaLeituraWeb.WebApp.Compartilhado.Infra;
+
+public interface IRepositorio<T> where T : EntidadeBase<T>
+{
+    void Cadastrar(T entidade);
+    bool Editar(string idSelecionado, T entidadeAtualizada);
+    bool Excluir(T registro);
+    T? SelecionarPorId(string idSelecionado);
+    List<T> SelecionarTodos();
+    List<T> Filtrar(Predicate<T> filtro);
+}
